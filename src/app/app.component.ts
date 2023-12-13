b6af926b5e56ca2beb574e8ddb87a1c05ef2f968
nguyennhut <nhut.nguyen@xomad.com>
2023-12-13 18:05:26 +0700

    feat(header): implement basic header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { OpButtonDirective } from '../@core/directives/op-button.directive';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, OpButtonDirective],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'open-fashion';
 }
